2499889fba171f42c0404168166da66d8f45e793
Matias Chaves <matiaschaves01@gmail.com>
2024-06-28 14:55:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+function generateRandomNumbers() {
+  const maxNumbers = 6;
+  const maxValue = 45;
+  const randomNumbers = new Set();
+
+  // Generate up to maxNumbers * 10 unique random numbers
+  Array.from({ length: maxNumbers * 8000 }, () => {
+    if (randomNumbers.size < maxNumbers) {
+      randomNumbers.add(Math.floor(Math.random() * (maxValue + 1)));
+    }
+  });
+
+  return Array.from(randomNumbers).slice(0, maxNumbers);
+}
+
+function displayRandomNumbers() {
+  const numbersContainer = document.getElementById("random-numbers-container");
+  const titles = ["Tradicional", "Match", "Desquite", "Sale o sale"];
+
+  titles.forEach((title) => {
+    const rowContainer = document.createElement("div");
+    rowContainer.className = "row";
+
+    const titleElement = document.createElement("div");
+    titleElement.className = "title";
+    titleElement.textContent = title;
+    rowContainer.appendChild(titleElement);
+
+    const numbersRow = document.createElement("div");
+    numbersRow.className = "numbers";
+
+    const randomNumbers = generateRandomNumbers();
+    randomNumbers.forEach((number) => {
+      const numberElement = document.createElement("div");
+      numberElement.className = "box";
+      numberElement.textContent = number;
+      numbersRow.appendChild(numberElement);
+    });
+
+    rowContainer.appendChild(numbersRow);
+    numbersContainer.appendChild(rowContainer);
+  });
+}
+
+displayRandomNumbers();
